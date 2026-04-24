e05124875e70b80ef396168e223843afecef9afd
Niels Stauss <s88665@htw-dresden.de>
2026-04-24 22:07:12 +0200

    tried to add the qustion overlay when clicking a button of a category still does not work yet

--- js/app.js
+++ js/app.js
@@ -3,46 +3,71 @@ const matheBtn = document.getElementById("mathe");
 const deutschBtn = document.getElementById("deutsch");
 const informatikBtn = document.getElementById("informatik");
 const webBtn = document.getElementById("web");
-const selscreen = Document.getElementById("category-selection");
+const selscreen = document.getElementById("category-selection");
 const quiz = document.getElementById("quiz");
+let allQuestions = {};
+let currentQuestions = [];
+let currentQuestionIndex = 0;
+const frageObjekt = currentQuestions[0];
+category = 
+
+
+async function loadQuestions() {
+    try {
+        const response = await fetch('questions.json');
+        allQuestions = await response.json();
+    } catch (e) { console.error("JSON konnte nicht geladen werden", e); }
+}
 
 function switchmathquiz()
 {
   selscreen.style.display = "none";
-  quiz.style.display = "block";
+  quiz.style.display = "visible";
 
   showQuestion("mathe");
-  updateProgress();
 }
 
 function switchdeutschquiz()
 {
   selscreen.style.display = "none";
-  quiz.style.display = "block";
+  quiz.style.display = "visible";
   
   showQuestion("deutsch");
-  updateProgress();
 }
 
 function switchinformatikquiz()
 {
   selscreen.style.display = "none";
-  quiz.style.display = "block";
+  quiz.style.display = "visible";
   
   showQuestion("informatik");
-  updateProgress();
 }
 
 function switchwebquiz()
 {
   selscreen.style.display = "none";
-  quiz.style.display = "block";
+  quiz.style.display = "visible";
+
   
   showQuestion("web");
-  updateProgress();
 }
 
+function showQuestion(category)
+{
+  currentQuestions = allQuestions[category];
+  currentQuestionIndex = 0;
+  frageObjekt = currentQuestions[currentQuestionIndex];
+  document.getElementById("question").innerText = frageObjekt.question;
+
+}
+loadQuestions();
 matheBtn.addEventListener("click", switchmathquiz);
 deutschBtn.addEventListener("click", switchdeutschquiz);
 informatikBtn.addEventListener("click", switchinformatikquiz);
-webBtn.addEventListener("click", switchwebquiz);
+webBtn.addEventListener("click", switchwebquiz);async function loadQuestions() {
+    try {
+        const response = await fetch('questions.json');
+        allQuestions = await response.json();
+    } catch (e) { console.error("JSON konnte nicht geladen werden", e); }
+}
+loadQuestions();
