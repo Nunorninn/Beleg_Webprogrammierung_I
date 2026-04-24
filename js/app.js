d714f6ca279384efb19a5248dd3d8fc2e62e5389
Niels Stauss <s88665@htw-dresden.de>
2026-04-24 23:08:41 +0200

    weitere Test am hinzufügen des Textes mit veränderung der Button Größe

--- js/app.js
+++ js/app.js
@@ -5,24 +5,33 @@ const informatikBtn = document.getElementById("informatik");
 const webBtn = document.getElementById("web");
 const selscreen = document.getElementById("category-selection");
 const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const Anworten = document.getElementById("answers");
+
 let allQuestions = {};
 let currentQuestions = [];
 let currentQuestionIndex = 0;
 const frageObjekt = currentQuestions[0];
-category = 
 
+//Elemente für die Antworten
+const ans1 = document.getElementById("answer1");
+const ans2 = document.getElementById("answer2");
+const ans3 = document.getElementById("answer3");
+const ans4 = document.getElementById("answer4");
 
 async function loadQuestions() {
     try {
-        const response = await fetch('questions.json');
+        const response = await fetch('data/questions.json');
         allQuestions = await response.json();
+        console.log("Daten geladen:", allQuestions);
     } catch (e) { console.error("JSON konnte nicht geladen werden", e); }
 }
+loadQuestions();
 
 function switchmathquiz()
 {
   selscreen.style.display = "none";
-  quiz.style.display = "visible";
+  quiz.style.display = "block";
 
   showQuestion("mathe");
 }
@@ -30,7 +39,7 @@ function switchmathquiz()
 function switchdeutschquiz()
 {
   selscreen.style.display = "none";
-  quiz.style.display = "visible";
+  quiz.style.display = "block";
   
   showQuestion("deutsch");
 }
@@ -38,7 +47,7 @@ function switchdeutschquiz()
 function switchinformatikquiz()
 {
   selscreen.style.display = "none";
-  quiz.style.display = "visible";
+  quiz.style.display = "block";
   
   showQuestion("informatik");
 }
@@ -46,28 +55,29 @@ function switchinformatikquiz()
 function switchwebquiz()
 {
   selscreen.style.display = "none";
-  quiz.style.display = "visible";
-
+  quiz.style.display = "block";
   
   showQuestion("web");
 }
 
 function showQuestion(category)
 {
-  currentQuestions = allQuestions[category];
+  /*currentQuestions = allQuestions[category];
   currentQuestionIndex = 0;
   frageObjekt = currentQuestions[currentQuestionIndex];
-  document.getElementById("question").innerText = frageObjekt.question;
+  const questionElement = document.getElementById("question");
+  questionElement.innerText = frageObjekt.a;
+  */
+ currentQuestion =  allQuestions[category][currentQuestionIndex];
+  question.innerText = currentQuestion.a;
+  ans1.value = currentQuestion.l[0];
+  ans2.value = currentQuestion.l[1];
+  ans3.value = currentQuestion.l[2];
+  ans4.value = currentQuestion.l[3];
 
 }
-loadQuestions();
+
 matheBtn.addEventListener("click", switchmathquiz);
 deutschBtn.addEventListener("click", switchdeutschquiz);
 informatikBtn.addEventListener("click", switchinformatikquiz);
-webBtn.addEventListener("click", switchwebquiz);async function loadQuestions() {
-    try {
-        const response = await fetch('questions.json');
-        allQuestions = await response.json();
-    } catch (e) { console.error("JSON konnte nicht geladen werden", e); }
-}
-loadQuestions();
+webBtn.addEventListener("click", switchwebquiz);
