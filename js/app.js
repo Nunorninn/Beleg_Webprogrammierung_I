d1264ea7faeb7787a8da0076520e0bf8507ec769
Niels Stauss <s88665@htw-dresden.de>
2026-05-06 10:05:02 +0200

    Shuffle der fragen und komprimierung der Funktion mit richtiger Antwortauswertung

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ let currentQuestions = [];
 let currentQuestionIndex = 0;
 let EndRes = 0;
 let Richtig = 0;
+let correctAnswerHTML = "";
 const frageObjekt = currentQuestions[0];
 
 //Elemente für die Antworten
@@ -69,15 +70,36 @@ function switchwebquiz()
   showQuestion("web");
 }
 
+function shuffleAnswers(content)
+{
+  for (let i = content.length - 1; i > 0; i--)
+  {
+    const rand = Math.floor(Math.random() * (i+1));
+    [content[i], content[rand]] = [content[rand],content[i]]; 
+  }
+  return content;
+}
+
+
+
 function showQuestion(category)
 {
+
   currentcategory = category;
   currentQuestion =  allQuestions[category][currentQuestionIndex];
+  correctAnswerHTML = currentQuestion.l[0];
+
+  let shuffledAns = [currentQuestion.l[0],currentQuestion.l[1],currentQuestion.l[2],currentQuestion.l[3]];
+  shuffleAnswers(shuffledAns);
   question.innerHTML = currentQuestion.a;
-  ans1.innerHTML = currentQuestion.l[0];
-  ans2.innerHTML = currentQuestion.l[1];
-  ans3.innerHTML = currentQuestion.l[2];
-  ans4.innerHTML = currentQuestion.l[3];
+  ans1.innerHTML = shuffledAns[0];
+  ans1.setAttribute("data-raw", shuffledAns[0]);
+  ans2.innerHTML = shuffledAns[1];
+  ans2.setAttribute("data-raw", shuffledAns[1]);
+  ans3.innerHTML = shuffledAns[2];
+  ans3.setAttribute("data-raw", shuffledAns[2]);
+  ans4.innerHTML = shuffledAns[3];
+  ans4.setAttribute("data-raw", shuffledAns[3]);
 
   renderMathInElement(document.getElementById("answers"), {
         delimiters: [
@@ -88,68 +110,28 @@ function showQuestion(category)
 
 }
 
-function Antwort1()
-{ 
-  if(currentQuestionIndex>=10){return;}
-  if(ans1.innerHTML==currentQuestion.l[0]) {EndRes++; Richtig=1;}
-  currentQuestionIndex++;
-  Updateprogress(currentQuestionIndex);
-  if(currentQuestionIndex >= 10)
-  {
-    ShowResult(Richtig);
-  }
-  else{
-    showQuestion(currentcategory);
-  }
-  
-}
+function checkAnswer(button) {
+  if (currentQuestionIndex >= 10) return;
 
-function Antwort2()
-{
-  if(currentQuestionIndex>=10){return;}
-  if(ans2.innerHTML==currentQuestion.l[0]) {EndRes++; Richtig=1;}
-  currentQuestionIndex++;
-  Updateprogress(currentQuestionIndex);
-  if(currentQuestionIndex >= 10)
-  {
-    ShowResult(Richtig);
-  }
-  else{
-    showQuestion(currentcategory);
-  }
-  
-}
+  const selAnsw = button.getAttribute("data-raw"); // KI gegeben um den Inhalt zu checken wenn es mit Katex verändert wurde
 
-function Antwort3()
-{
-  if(currentQuestionIndex>=10){return;}
-  if(ans3.innerHTML==currentQuestion.l[0]) {EndRes++; Richtig=1;}
-  currentQuestionIndex++;
-  Updateprogress(currentQuestionIndex);
-  if(currentQuestionIndex >= 10)
-  {
-    ShowResult(Richtig);
+  if (selAnsw == correctAnswerHTML) {
+    EndRes++;
+    Richtig = 1;
+    console.log("Antwort war Richtig!");
+  } else {
+    Richtig = 0;
+    console.log("Antwort war falsch!");
   }
-  else{
-    showQuestion(currentcategory);
-  }
-  
-}
 
-function Antwort4()
-{
-  if(currentQuestionIndex>=10){return;}
-  if(ans4.innerHTML==currentQuestion.l[0]) {EndRes++; Richtig=1;}
   currentQuestionIndex++;
   Updateprogress(currentQuestionIndex);
-  if(currentQuestionIndex >= 10)
-  {
-    ShowResult(Richtig);
-  }
-  else{
+
+  if (currentQuestionIndex >= 10) {
+      ShowResult(Richtig);
+  } else {
     showQuestion(currentcategory);
   }
-  
 }
 
 function goBack() {
@@ -190,7 +172,7 @@ matheBtn.addEventListener("click", switchmathquiz);
 deutschBtn.addEventListener("click", switchdeutschquiz);
 informatikBtn.addEventListener("click", switchinformatikquiz);
 webBtn.addEventListener("click", switchwebquiz);
-ans1.addEventListener("click", Antwort1);
-ans2.addEventListener("click", Antwort2);
-ans3.addEventListener("click", Antwort3);
-ans4.addEventListener("click", Antwort4);
+ans1.addEventListener("click", function() {checkAnswer(this)});
+ans2.addEventListener("click", function() {checkAnswer(this)});
+ans3.addEventListener("click", function() {checkAnswer(this)});
+ans4.addEventListener("click", function() {checkAnswer(this)});
