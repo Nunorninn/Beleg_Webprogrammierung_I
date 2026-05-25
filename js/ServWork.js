7a17aa415d9aa49c569f4e8cd4870c3d6eb4e43a
unknown <you@example.com>
2026-05-25 22:49:42 +0200

    changed ServWork and manifest.json

--- js/ServWork.js
+++ js/ServWork.js
@@ -10,7 +10,16 @@ const ASSETS = [
     'https://cdn.jsdelivr.net/npm/vexflow@4.2.2/build/cjs/vexflow.js',
     'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.min.js',
     'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
-    'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js'
+    'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js',
+    'data/saveHighscore.php',
+    'data/getHighSC.php',
+    'data/getWebQuest.php',
+    'data/solve.php',
+    'data/db.php',
+    'ServWork.js',
+    'highscores.sqlite',
+    'audio/rightanswer.mp3',
+    'audio/wronganswer.mp3'
 ];
 
 //Offline Inhalt für den Cache
@@ -26,8 +35,8 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((reponse) => {
-            return reponse || fetch(event.request);
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
         })
     );
 });
